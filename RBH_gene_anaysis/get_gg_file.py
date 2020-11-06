e4549a65edde6fa3806e32671d66ec4406bc7118
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 20:12:05 +0800

    Update get_gg_file.py

--- RBH_gene_anaysis/get_gg_file.py
+++ RBH_gene_anaysis/get_gg_file.py
@@ -1,6 +1,6 @@
-#!/public/agis/chengshifeng_group/fuyuan/pip-fuyuan/app/anaconda2/bin/python
+#!app/anaconda2/bin/python
 # -*- coding: UTF-8 -*-
-# Author: fuyuan (907569282@qq.com)
+# Author: Yuan-SW-F, yuanswf@163.com
 # Created Time: 2019-05-15 09:11:13
 # Example get_gg_file.py   
 import sys, os, re
