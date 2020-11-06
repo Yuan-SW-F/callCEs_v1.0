f3f0ca78508634a7e51d716695b477db23df7d90
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 20:37:38 +0800

    Update forspcne.sh

--- shell/forspcne.sh
+++ shell/forspcne.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # File Name: forspcne.sh
-# Author  : fuyuan, 907569282@qq.com
+# Author  : Yuan-SW-F, yuanswf@163.com
 # Created Time: 2020-03-14 14:15:54
 source ~/.bashrc
 
@@ -24,7 +24,7 @@ for i in `cut -f 1 $R | perl -ne 'print "$1\n" if /(\w\w\w\w\w)$/' | sort | uniq
 		grep \$j $R > tmp/$i.bed
 		sortBed -i tmp/$i.bed > tmp/$i.sorted.bed
 		mergeBed -i tmp/$i.sorted.bed -d 3 > tmp/$i.sorted.merged.bed
-		perl /vol3/asbc/CHENGSHIFENG_GROUP/fuyuan/02.Nfix/00.81species_CNE/23.CNE/03.New_ann/05.compare/04.each_CNE/03.idbed/perl.pl $R $i
+		perl idbed.pl $R $i
 		#head -n 1 tmp/$i.sorted.bed | cut -f 4-6 > tmp/add
 		#perl -ne 'chomp; @l = split /\\t/; \$n = \`head -n 1 tmp/add\`; print join \"\\t\", @l, \$n;' tmp/$i.sorted.merged.bed >> bed/$R-$i.bed
 	done
