ac015b8b899d84cec7e951b7eb4bda240bf07afd
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 20:24:30 +0800

    Update Step2.2.class_prepare_bed.sh

--- shell/Step2.2.class_prepare_bed.sh
+++ shell/Step2.2.class_prepare_bed.sh
@@ -4,8 +4,8 @@
 # Created Time: 2020-02-20 12:37:08
 source ~/.bashrc
 mkdir $1
-ln -s /vol3/asbc/CHENGSHIFENG_GROUP/fuyuan/02.Nfix/00.81species_CNE/23.CNE/01.class/$1/*.score-0.9.sorted $1
-cp /vol3/asbc/CHENGSHIFENG_GROUP/fuyuan/02.Nfix/00.81species_CNE/23.CNE/01.class/$1/*maf $1
+ln -s ..//01.class/$1/*.score-0.9.sorted $1
+cp ../01.class/$1/*maf $1
 
 cd $1
 for i in `ls *.score-0.9.sorted |sed s/.score-0.9.sorted//`; do  mergeBed -d 3 -i $i.score-0.9.sorted  | sed s/Medicago_truncatula.// > $i.cons.bed; sortBed -i $i.cons.bed > $i.cons.bed.sorted ; mergeBed -i $i.cons.bed.sorted | awk '{if ($3-$2>=5){print}}' > $i.cons.bed.sorted.merged.bed;done
