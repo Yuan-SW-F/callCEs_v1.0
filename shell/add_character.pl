5aaa3d6f4a919837bd666cd074efb1d8a68f5e46
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2020-11-06 20:31:50 +0800

    Update add_character.pl

--- shell/add_character.pl
+++ shell/add_character.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 =head1 Info
-    Script Author  : fuyuan, 907569282@qq.com
+    Script Author  : Yuan-SW-F, yuanswf@163.com
     Created Time   : 2020-03-10 17:00:40
     Example: add_clade.pl
 =cut
