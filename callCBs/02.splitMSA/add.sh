ab32386c75c01995488de40bde474d72c3c95661
Yuan-SW-F <49896080+Yuan-SW-F@users.noreply.github.com>
2021-03-31 09:29:49 +0800

    Create add.sh

---
+++ callCBs/02.splitMSA/add.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# File Name: add.sh
+# Author  : yuan, 
+# Created Time: 2020-03-09 03:18:27
+source ~/.bashrc
+
+for i in MtrunA17Chr0c07_Metru MtrunA17Chr0c08_Metru MtrunA17Chr0c11_Metru MtrunA17Chr0c13_Metru MtrunA17Chr0c16_Metru MtrunA17Chr0c21_Metru MtrunA17Chr0c23_Metru MtrunA17Chr0c24_Metru MtrunA17Chr0c25_Metru MtrunA17Chr0c26_Metru;do
+        perl -ne 'chomp; print "$_\tDistal\tUnknown\t.\t.\t.\t.\n"' $i.bed > $i.bed.stream
+done
